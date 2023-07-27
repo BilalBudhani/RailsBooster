982faa96b2963509604bd008e4e96ef3389215bc
bilal <bilal@bilalbudhani.com>
2023-07-27 13:40:42 +0400

    Adds GoodJob for backgrounding

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,7 @@ module RailsBooster
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.active_job.queue_adapter = :good_job
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  mount GoodJob::Engine => 'good_job'
+
   # Defines the root path route ("/")
   # root "articles#index"
 end
