7f4716dd42df078f81a02bfb2bb8d6ffcfe15c97
bilal <bilal@bilalbudhani.com>
2025-10-20 12:42:29 +0400

    Adds flash messaging component

---
+++ app/frontend/components/common/Flash.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { computed } from 'vue'
+import { CircleAlert, CircleCheck } from 'lucide-vue-next';
+
+const props = defineProps({
+  flash: {
+    type: Object,
+  },
+})
+
+const showFlash = computed(() => {
+  return props.flash.success || props.flash.alert
+})
+</script>
+<template>
+  <section class="text-sm text-center mb-4" v-if="showFlash">
+
+    <div class="rounded-md px-2 py-1 font-medium border inline-flex items-center justify-center" role="alert" :class="{
+      'text-green-800 bg-green-50': flash.success,
+      'text-red-800 bg-red-50': flash.alert,
+    }">
+      <CircleAlert class="w-4 h-4" v-if="flash.alert" />
+      <CircleCheck class="w-4 h-4" v-if="flash.success" />
+      <span class="ml-2">{{ flash?.success || flash?.alert }}</span>
+    </div>
+
+  </section>
+
+</template>
--- app/frontend/components/layouts/Layout.vue
+++ app/frontend/components/layouts/Layout.vue
@@ -1,10 +1,18 @@
 <script setup>
 import Header  from '@/components/Common/Header.vue'
+import Flash from '@/components/Common/Flash.vue'
+
+defineProps({
+  flash: {
+    type: Object
+  },
+})
 </script>
 
 <template>
   <Header />
   <main class="container mx-auto max-w-xl py-12">
+    <Flash :flash="flash" />
     <slot />
   </main>
 </template>
