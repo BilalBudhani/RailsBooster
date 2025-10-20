91cf888abb4cafc8c593c2e8b85311bf2faf7504
bilal <bilal@bilalbudhani.com>
2025-10-20 13:01:20 +0400

    small UI improvements

--- app/frontend/pages/Main.vue
+++ app/frontend/pages/Main.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { Link, Head } from '@inertiajs/vue3'
 import Layout from '@/components/Layouts/Layout.vue'
+import { Rocket } from "lucide-vue-next"
 
 defineOptions({
   layout: Layout,
@@ -17,9 +18,12 @@ defineProps({
   <Head title="Welcome!" />
 
   <div class="text-center">
-    <h1 class="text-6xl leading-tight italic font-black"> 🚀 RailsBooster</h1>
+    <h1 class="text-6xl leading-tight font-black inline-flex gap-2 items-center justify-center">
+      <Rocket class="w-16 h-16 inline-block" />
+      <span>RailsBooster</span>
+    </h1>
 
-    <p class="text-xl text-slate-700">Preconfigured Ruby On Rails Boilerplate To Provide Instant Productivity ⚡️</p>
+    <p class="text-xl text-slate-700">Ruby On Rails Starter Kit For Instant Productivity</p>
 
     <div class="py-6" v-if="user">
       <strong>Welcome {{ user.first_name }}!</strong>
